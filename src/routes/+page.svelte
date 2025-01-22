d2f7c36212cbacebfe8c65232458a338afd80feb
Ben Cooper <cooperben83@gmail.com>
2025-01-22 23:03:28 +0000

    moved to sveltekit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="window">
+	<div class="title-bar">
+		<span class="title">PiCNiC CLUB</span>
+		<div class="buttons">
+			<div class="button close"></div>
+			<div class="button minimize"></div>
+			<div class="button maximize"></div>
+		</div>
+	</div>
+	<div class="content">
+		<p>Welcome to the Mac OS System 1.0!</p>
+	</div>
+</div>
