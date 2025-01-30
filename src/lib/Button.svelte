0fc4148322e71368a5926b33cac38baf31c66d2f
Ben Cooper <cooperben83@gmail.com>
2025-01-30 15:28:20 +0000

    fun cool stuff - love Ben

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
 	let { children } = $props();
+
+	let confetti = $state(false);
+
+	let confettiStart = $state([0, 0]);
+	const confettiDuration = 350;
+
+	function throwConfetti(event: { clientX: number; clientY: number }) {
+		confetti = true;
+		confettiStart = [event.clientX, event.clientY];
+
+		setTimeout(() => {
+			confetti = false;
+		}, confettiDuration);
+	}
 </script>
 
-<button>
-	{@render children()}
-</button>
+<div class="relative">
+	<button onclick={throwConfetti}>
+		{@render children()}
+	</button>
+
+	{#if confetti}
+		<div class="confetti">
+			{#each new Array(100) as _, i}
+				<span
+					class="piece"
+					style="--start-x: {confettiStart[0]}px; --start-y: {confettiStart[1]}px; --translate-x: {Math.random() *
+						200 -
+						100}px; --translate-y: {Math.random() * 200 - 100}px;"
+				></span>
+			{/each}
+		</div>
+	{/if}
+</div>
 
 <style lang="scss">
 	button {
@@ -20,5 +49,37 @@
 	button:hover {
 		background-color: $red-color;
 		color: white;
+	}
+
+	.confetti {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		pointer-events: none;
+	}
+
+	.piece {
+		position: fixed;
+		width: 10px;
+		height: 10px;
+		background-color: $red-color;
+		border-radius: 50%;
+		pointer-events: none;
+		top: var(--start-y);
+		left: var(--start-x);
+		animation: confetti 350ms ease-out forwards;
+	}
+
+	@keyframes confetti {
+		0% {
+			opacity: 1;
+			transform: translate(0, 0);
+		}
+		100% {
+			opacity: 0;
+			transform: translate(var(--translate-x), var(--translate-y));
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,45 @@
-<div class="window">
-	<div class="title-bar">
-		<span class="title">PiCNiC CLUB</span>
-		<div class="buttons">
-			<div class="button close"></div>
-			<div class="button minimize"></div>
-			<div class="button maximize"></div>
+<script lang="ts">
+	import Button from '$lib/Button.svelte';
+
+	let snips = ['foo', 'bar', 'baz', 'foo', 'bar', 'baz', 'foo', 'bar', 'baz'];
+	let website = 'https://www.google.com';
+</script>
+
+{#snippet window(title: string, src: string)}
+	<div class="window">
+		<div class="title-bar">
+			<span class="title">{title}</span>
+			<div class="buttons">
+				<div class="button close"></div>
+				<div class="button minimize"></div>
+				<div class="button maximize"></div>
+			</div>
+		</div>
+		<div class="content">
+			<iframe {title} {src} frameborder="0"></iframe>
 		</div>
 	</div>
-	<div class="content">
-		<p>Welcome to the Mac OS System 1.0!</p>
-	</div>
+{/snippet}
+
+<div class="bento">
+	{#each snips as snip}
+		{@render window(snip, website)}
+	{/each}
 </div>
 
+<Button>Click me</Button>
+
 <style lang="scss">
+	.bento {
+		display: grid;
+		grid-template-columns: repeat(3, minmax(300px, 1fr));
+		grid-auto-rows: minmax(150px, auto);
+		gap: 20px;
+	}
+
 	.window {
-		width: 300px;
+		width: 100%;
+		height: 100%;
 		border: 2px solid $red-color;
 		box-shadow: 4px 4px 0 darken($red-color, 5%);
 	}
